43df6615c157f0634fbc2965d11ba5b645626ccd
KavyaGJS <kavyamujk@gmail.com>
2024-08-13 13:04:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nlpnew',  
-    version='v0.1.0',  
+    version='0.0.1',  
     description='python package',
     url='https://github.com/KavyaGJS/pypack',  
     author='KavyaGJS', 
